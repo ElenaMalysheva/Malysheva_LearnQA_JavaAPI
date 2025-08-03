70b4d960d2ce68f0e1f8c96b681dc3952f9af770
ElenaMalysheva <photo.malysheva@gmail.com>
2025-08-03 17:38:26 +0400

    video 2.2

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-public class HelloWorldTest {
+/*public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
@@ -12,10 +12,28 @@ public class HelloWorldTest {
                 .andReturn();
         response.prettyPrint();*/
 
-        Response response = RestAssured
+        /*Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
 
+    }/*
+
+         */
+
+
+
+public class HelloWorldTest {
+
+    @Test // второе занятие
+    public void testRestAssure(){ // это билдер
+        Response response = RestAssured //переменная, которую мы использовали в первой строке
+                .get("https://playground.learnqa.ru/api/hello") // вызываем методы, это параметр/ сеттер
+                .andReturn(); // это функция, исполнитель
+        response.prettyPrint();
+
     }
+
+
+
 }
