babcb55e05dc6c5bdb9c802cbaad40346e7c8c9b
ElenaMalysheva <photo.malysheva@gmail.com>
2025-08-25 21:17:02 +0300

    video 3.1 JUnit и простые тесты

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -7,62 +7,30 @@ import io.restassured.response.Response;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class HelloWorldTest {
 
     @Test
-    public void testRestAssure(){ // это билдер
-        Map<String,Object> data = new HashMap<>();
-        data.put("login", "secret_login2"); //задачем параметры
-        data.put("password", "secret_pass2 ");
+    public void testFor200(){ // это билдер
 
-
-
-        Response responseForGet = RestAssured
-                .given()
-                //.headers(headers)
-                .body(data)
-                .when()
-                .post ("https://playground.learnqa.ru/api/get_auth_cookie") //
+        Response response = RestAssured
+                .get ("https://playground.learnqa.ru/api/map") //
                 .andReturn();
-
-        String responseCookie = responseForGet.getCookie("auth_cookie");
-
-        Map<String,String> cookies = new HashMap<>();
-
-        if (responseCookie != null){
-            cookies.put("auth_cookie",responseCookie);
+        //assertTrue(response.statusCode()==200, "unexpected status code"); //проверяет булевое значение
+        assertEquals(200, response.statusCode(), "unexpected status code"); //увидим ожидаемое и фактическое значение
         }
 
-        Response responseForCheck = RestAssured
-                .given()
-                .body(data)
-                .cookies(cookies)
-                .when()
-                .post("https://playground.learnqa.ru/api/check_auth_cookie")
+    @Test
+    public void testFor404() { // это билдер
+
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map2") //
                 .andReturn();
-
-        responseForCheck.print();
-
-        /* System.out.println("\nPretty text:"); // печатаем полный ответ ( будет пустым)
-        response.prettyPrint();
-
-        System.out.println("\nHeaders:");
-        Headers responseHeaders = response.getHeaders();
-        System.out.println(responseHeaders); // печатаем все хедерс
-
-        System.out.println("\nCookies:");
-        Map<String,String> responseCookies = response.getCookies();
-        System.out.println(responseCookies); // печатаем куки */
-
-        //String responseCookie = response.getCookie("auth_cookie");
-        //System.out.println(responseCookie); // вывод только куки
-
-
-
+        assertEquals(404, response.statusCode(), "unexpected status code");
+    }
 
 
     }
-
-}
--- src/test/java/HomeWork9.java
+++ src/test/java/HomeWork9.java
@@ -19,12 +19,12 @@ public class HomeWork9 {
                 "1234", "qwertyuiop", "admin", "letmein", "welcome",
                 "monkey", "iloveyou", "123321", "1qaz2wsx", "sunshine"
         );
-        for (String password : passwordList) {
+        for (String password : passwordList) { // запускаем цикл
             Map<String, String> data = new HashMap<>();
-            data.put("login", "super_admin");
+            data.put("login", "super_admin"); // объявляем переменные
             data.put("password", password);
 
-        Response response = RestAssured
+        Response response = RestAssured // первый запрос для получения куки
                 .given()
                 .body(data)
                 .when()
@@ -32,23 +32,23 @@ public class HomeWork9 {
                 .andReturn();
 
 
-        // Получение cookie
+        // Сохраняем и печатаем cookie
         String responCookie = response.getCookie("auth_cookie");
         System.out.println("auth_cookie: " + responCookie);
 
 
-        Response responseSecond = RestAssured //запрос с token ДО того, как задача готова
+        Response responseSecond = RestAssured //Второй запрос, с использованием куки
 
                 .given()
                 .cookie("auth_cookie", responCookie)
                 .get("https://playground.learnqa.ru/ajax/api/check_auth_cookie") //
                 .andReturn();
-        
 
-        String secondResponse = responseSecond.asString();
+
+        String secondResponse = responseSecond.asString(); // записываем и печатаем результат запроса
             System.out.println(secondResponse);
 
-            if (!secondResponse.contains("You are NOT authorized")) {
+            if (!secondResponse.contains("You are NOT authorized")) { // ищем условие и прерываем цикл при его выполнении
                 System.out.println("✅ Найден правильный пароль: " + password);
                 System.out.println("➡ Ответ сервера: " + secondResponse);
                 break;
@@ -58,6 +58,60 @@ public class HomeWork9 {
         }
 
 /*
+Ex9: Подбор пароля
+Сегодня к нам пришел наш коллега и сказал, что забыл свой пароль от важного сервиса. Он просит нас помочь ему написать программу, которая подберет его пароль.
+
+
+
+Условие следующее. Есть метод: https://playground.learnqa.ru/ajax/api/get_secret_password_homework
+
+
+Его необходимо вызывать POST-запросом с двумя параметрами: login и password
+
+
+
+Если login указан и существует, метод вернет нам авторизационную cookie с названием auth_cookie и каким-то значением.
+
+
+У метода существует защита от перебора. Если верно указано поле login, но передан неправильный password, то авторизационная cookie все равно вернется. НО с "неправильным" значением, которое на самом деле не позволит создавать авторизованные запросы. Только если и login, и password указаны верно, вернется cookie с "правильным" значением. Таким образом используя только метод get_secret_password_homework невозможно узнать, передали ли мы верный пароль или нет.
+
+
+
+По этой причине нам потребуется второй метод, который проверяет правильность нашей авторизованной cookie: https://playground.learnqa.ru/ajax/api/check_auth_cookie
+
+
+
+Если вызвать его без cookie с именем auth_cookie или с cookie, у которой выставлено "неправильное" значение, метод вернет фразу "You are NOT authorized".
+
+
+Если значение cookie “правильное”, метод вернет: “You are authorized”
+
+
+Коллега говорит, что точно помнит свой login - это значение super_admin
+
+
+
+А вот пароль забыл, но точно помнит, что выбрал его из списка самых популярных паролей на Википедии (вот тебе и супер админ...).
+
+
+Ссылка: https://en.wikipedia.org/wiki/List_of_the_most_common_passwords
+
+Искать его нужно среди списка Top 25 most common passwords by year according to SplashData - список паролей можно скопировать в ваш тест вручную или придумать более хитрый способ, если сможете.
+
+
+
+Итак, наша задача - написать тест и указать в нем login нашего коллеги и все пароли из Википедии в виде списка. Программа должна делать следующее:
+
+
+1. Брать очередной пароль и вместе с логином коллеги вызывать первый метод get_secret_password_homework. В ответ метод будет возвращать авторизационную cookie с именем auth_cookie и каким-то значением.
+
+
+2. Далее эту cookie мы должна передать во второй метод check_auth_cookie. Если в ответ вернулась фраза "You are NOT authorized", значит пароль неправильный. В этом случае берем следующий пароль и все заново. Если же вернулась другая фраза - нужно, чтобы программа вывела верный пароль и эту фразу.
+
+
+Ответом к задаче должен быть верный пароль и ссылка на коммит со скриптом.
+
+
                  */
 
 
